07b4a2215816c89c615a27bc8e46f37c91b3427c
= <cschreib@orange.fr>
2016-02-11 11:33:07 +0100

    Made fill_nan more flexible in input cube format

--- fill_nan.cpp
+++ fill_nan.cpp
@@ -6,14 +6,20 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
-    vec3f cube;
+    vec3d cube;
     fits::image img(argv[1]);
-    img.read(cube);
+    for (uint_t i : range(img.hdu_count())) {
+        img.reach_hdu(i);
+        if (img.axis_count() == 3 && total(img.image_dims()) != 0) {
+            img.read(cube);
+            break;
+        }
+    }
 
     for (uint_t l : range(cube.dims[0])) {
         auto mima = minmax(cube(l,_,_));
         if (mima.first == mima.second) {
-            cube(l,_,_) = fnan;
+            cube(l,_,_) = dnan;
         }
     }
 
