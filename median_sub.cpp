6d9afc3af8ee04c9ec735609f785b2eba721da6d
cschreib <cschreib@orange.fr>
2016-05-24 14:11:55 +0200

    Can now mask regions to exclude when median subtracting the cube

--- median_sub.cpp
+++ median_sub.cpp
@@ -1,13 +1,26 @@
 #include <phypp.hpp>
 
-int main(int argc, char* argv[]) {
+int phypp_main(int argc, char* argv[]) {
     if (argc < 2) {
-        print("usage: median_sub <directory>");
+        print("usage: median_sub <directory> <mask_catalog>");
         return 0;
     }
 
     std::string dir = file::directorize(argv[1]);
     vec1s files = dir+file::list_files(dir+"sci_reconstructed*-sci.fits");
+
+    vec1d ra, dec, size;
+    if (argc > 2) {
+        vec1s id;
+        file::read_table(argv[2], file::find_skip(argv[2]), id, ra, dec, size);
+    }
+
+    auto ksigma = [](vec1d data) {
+        double v = median(data);
+        double m = 1.48*median(abs(data - v));
+        vec1u idg = where(abs(data - v) < 7*m);
+        return mean(data[idg]);
+    };
 
     for (auto oname : files) {
         std::string newfile = file::get_basename(oname);
@@ -19,8 +32,37 @@ int main(int argc, char* argv[]) {
 
             vec3d cube;
             fimg.read(cube);
+
+            uint_t ny = cube.dims[1];
+            uint_t nx = cube.dims[2];
+
+            vec2b mask = replicate(true, ny, nx);
+
+            // Mask nearby sources from the provided catalog (if any)
+            if (!ra.empty()) {
+                fits::wcs astro(fimg.read_header());
+                vec1d x, y;
+                fits::ad2xy(astro, ra, dec, x, y);
+                x -= 1.0; y -= 1.0;
+                double aspix;
+                fits::get_pixel_size(astro, aspix);
+                vec1d r = size/aspix;
+
+                vec2d ix = generate_img(mask.dims, [](int_t,    int_t tx) { return tx; });
+                vec2d iy = generate_img(mask.dims, [](int_t ty, int_t)    { return ty; });
+
+                for (uint_t s : range(ra)) {
+                    mask = mask && sqr(x[s] - ix) + sqr(y[s] - iy) > sqr(r[s]);
+                }
+            }
+
+            // Mask borders
+            mask(0,_) = mask(ny-1,_) = mask(_,0) = mask(_,nx-1) = false;
+
+            // Subtract whole IFU
+            vec1u idg = where(mask);
             for (uint_t l : range(cube.dims[0])) {
-                cube(l,_,_) -= median(cube(l,_,_));
+                cube(l,_,_) -= ksigma(cube(l,_,_)[idg]);
             }
 
             fimg.update(cube);
